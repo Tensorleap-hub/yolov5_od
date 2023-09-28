af0cbc357ab5db96e48039111ea8dbd4ec7a7193
RanHomri <69201624+RanHomri@users.noreply.github.com>
2023-09-28 09:27:48 +0300

    modify directory tree (#3)
    Co-authored-by: ranhomri <ran.homri@tensorleap.ai>

--- yolo_od/data/preprocessing.py
+++ yolo_od/data/preprocessing.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 def load_yolo_dataset(dataset_path, split) -> List[str]:
-    images_dir = os.path.join(dataset_path, 'images', split)
+    images_dir = os.path.join(dataset_path, split, 'images')
     images_names = os.listdir(images_dir)
     file_names = [file_name.rstrip('.jpg') for file_name in images_names]
     return file_names
